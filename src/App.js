19dcd0b468df24bcac968aa8fecff84c6056f8c4
Abrahm <abrahmrollins@gmail.com>
2024-05-20 17:18:36 +1200

    Removed second decleration of token variable - was resulting in undefined error

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import HeaderBar from'./components/header/HeaderBar';
 import InstagramFeed from './components/instafeed/InstagramPosts';
 
 function App() {
-  const token = process.env.INSTA_TOKEN;
   return (
     <div className="App">
       <HeaderBar />
