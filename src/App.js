9874b27d70a64023832dbd34cf8070e7759e326e
Abrahm <abrahmrollins@gmail.com>
2024-05-20 14:45:03 +1200

    Added code for post layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import HeaderBar from'./components/header/HeaderBar';
+import InstagramFeed from './components/instafeed/InstagramPosts';
 
 function App() {
+  const token = process.env.INSTA_TOKEN;
   return (
     <div className="App">
       <HeaderBar />
+      <InstagramFeed />
     </div>
   );
 }
---
+++ src/components/instafeed/InstagramPosts.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react';
+
+const InstagramFeed = () => {
+  const [feed, setFeed] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch(`https://graph.instagram.com/me/media?fields=id,caption,media_url,permalink&access_token=${INSTA_TOKEN}`)
+      .then(response => response.json())
+      .then(data => {
+        setFeed(data.data);
+      })
+      .catch(error => {
+        console.error(error);
+        setError(error);
+      });
+  }, [INSTA_TOKEN]);
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  return (
+    <div id="instagramFeed">
+      {feed.map(post => (
+        <div key={post.id} className="single-post">
+          <a href={post.permalink} target="_blank" rel="noopener noreferrer">
+            <img src={post.media_url} alt={post.caption} />
+          </a>
+          <p className="post-desc">{post.caption}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default InstagramFeed;
